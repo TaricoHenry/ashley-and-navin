5243372488e8796d2e2e04ab7e130f23e7c01be9
ethanl2 <ethanl@enet.gy>
2026-01-16 09:31:02 -0400

    Init Commit

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+document.getElementById("attend").addEventListener("change", () => {
+    //document.getElementById("attendance").classList.toggle("toggled")
+    document.getElementById("questions").classList.toggle("toggled")
+
+})
